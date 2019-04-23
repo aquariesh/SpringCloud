f304d4b1e9ccde39ef0833730365af6cd147fc47
wangjx <22042990@qq.com>
2019-04-23 10:56:28 +0800

    init commit

---
+++ spring-admin/src/main/java/known/springadmin/SpringAdminApplication.java
@@ -0,0 +1,18 @@
+package known.springadmin;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+
+
+@EnableDiscoveryClient
+@SpringBootApplication
+@EnableAdminServer
+public class SpringAdminApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringAdminApplication.class, args);
+    }
+
+}
